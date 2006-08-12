ba95b22c91981e12cff235b1dd931fce17e484c7
nakahiro <nakahiro@be8e08ad-ca19-0410-b3e9-fd3dc8ba15a0>
2006-08-12 02:42:17 +0000

    removed sql directory.  DDL can be generated with 'mvn process-classes' or 'ant generate.ddl' after merging hbm.xmlfragment into build.xml.

--- trunk/Coordinator/sql/llrps2.sql
+++
@@ -1,9 +0,0 @@
-drop table Agent;
-drop sequence hibernate_sequence;
-create table Agent (
-    id int8 not null,
-    ipAddress varchar(255),
-    name varchar(255),
-    primary key (id)
-);
-create sequence hibernate_sequence;
